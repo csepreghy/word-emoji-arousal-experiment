cc650e058085daca109875c3c2eb28dbb065b956
Andrew Chepreghy <andris788@gmail.com>
2019-05-22 10:47:11 +0200

    added random parameter as string

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from datasets import get_emoji_data
 import pandas as pd
 import matplotlib.pyplot as plt
 
-emoji_data = get_emoji_data()
+emoji_data = get_emoji_data('random_parameter')
 emoji_data_processed = process_emoji_daeta()
 
 word_clusters = run_kmeans(emoji_data_processed)
