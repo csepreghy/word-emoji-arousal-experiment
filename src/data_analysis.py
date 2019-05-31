2524f08c63bb99c21e5f70369020cf22125e9b9c
Andrew Chepreghy <andris788@gmail.com>
2019-05-31 17:36:57 +0200

    added all relevant data for individual stats

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -38,15 +38,21 @@ def read_data():
 
 def get_stats_for_every_word(df_list):
   correct_incorrect_answers = {}
+  response_times_mean = {}
+  response_times_std = {}
 
   for index, word in enumerate(list(df_list[0]['word'])):
     print(word)
     # correct_incorrect_answers = []
 
     correct_incorrect_answers[word] = []
+    response_times_mean[word] = []
+    response_times_std[word] = []
 
     for df in df_list:
-      # print('df[word][index]', df['word'][index])
+      response_times_mean[word].append(df['key_resp_7.rt'][index])
+      response_times_std[word].append(df['key_resp_7.rt'][index])
+
       if df['key_resp_7.keys'][index] == df['ground_truth'][index]:
         correct_incorrect_answers[word].append(1)
       
@@ -54,40 +60,39 @@ def get_stats_for_every_word(df_list):
         correct_incorrect_answers[word].append(0)
 
     correct_incorrect_answers[word] = np.mean(np.array(correct_incorrect_answers[word]))
+    response_times_mean[word] = np.mean(np.array(response_times_mean[word]))
+    response_times_std[word] = np.std(np.array(response_times_std[word]))
 
-  
-  print(len(list(correct_incorrect_answers.keys())))
-  print(len(list(correct_incorrect_answers.values())))
+  truth_list = list(df_list[0]['ground_truth'])
+  category_list = list(df_list[0]['category'])
+
   data = {
     'Word': list(correct_incorrect_answers.keys()),
-    'Mean Accuracy': list(correct_incorrect_answers.values())
+    'Mean Accuracy': list(correct_incorrect_answers.values()),
+    'Mean RT': list(response_times_mean.values()),
+    'Standard Deviation RT': list(response_times_std.values()),
+    'Truth': truth_list,
+    'Category': category_list
   }
+
+  print(list(response_times_mean.values()))
+  print(list(response_times_std.values()))
   word_stats_df = pd.DataFrame(data)
-  word_stats_df.to_csv('word_accuracies.csv')
+  word_stats_df.to_csv('word_stats.csv')
 
   print('word_stats_df.head()', word_stats_df)
-  # for df in df_list:
-  #   answers = list(df['key_resp_7.keys'])
-  #   ground_truth = list(df['ground_truth'])
-  #   response_times = list(df['key_resp_7.rt'])
-  #   words = list(df['word'])
-
-  #   for word in words:
 
 
+def individual_accuracy(df):
+  answers = list(df['key_resp_7.keys'])
+  response_times = list(df['key_resp_7.rt'])
+  ground_truth = list(df['ground_truth'])
 
-df_list = read_data()
+  n_correct = 0
 
-get_stats_for_every_word(df_list)
+  for answer, truth in zip(answers, ground_truth):
+    if answer == truth:
+      n_correct += 1
 
-
-
-#  answers = list(df['key_resp_7.keys'])
-#  response_times = list(df['key_resp_7.rt'])
-#  ground_truth = list(df['ground_truth'])
-
-# n_correct = 0
-
-# for answer, truth in zip(answers, ground_truth):
-#   if answer == truth:
-#     n_correct += 1
+# df_list = read_data()
+# get_stats_for_every_word(df_list)
